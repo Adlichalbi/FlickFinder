31f892fac114aef359cda135ef134c1e65797920
Adlichalbi <adlichalbi6612@gmail.com>
2025-01-26 18:09:54 +0100

    add Header and Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div> <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+   <main>
+    <div className='pattern'/>
+
+    <div className='wrapper'>
+
+      <header>
+
+        <img src='./hero.png' alt='Hero Banner'></img>
+        <h1>Find <span className='text-gradient'>Movies</span> You'lle Love
+         Without the hassle</h1>
+
+      </header>
+
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+    </div>
+   </main>
   )
 }
 
